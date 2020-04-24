8453e791dac71a56dbeacf333662c3c1a03d222f
WincherZ <wincher2016@gmail.com>
2020-04-24 21:37:36 +0300

    Updates

---
+++ precache-manifest.55d9089eeceee84d59a072b011849d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a0c74905eb0aea1d6d6b647bb4ace6",
+    "url": "/Wincherz/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "27b139204596da3728a2",
+    "url": "/Wincherz/react_static-list-of-todos/static/css/main.1f5bda3a.chunk.css"
+  },
+  {
+    "revision": "cdbfa4b0ed9a17660ba3",
+    "url": "/Wincherz/react_static-list-of-todos/static/js/2.b0f90b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wincherz/react_static-list-of-todos/static/js/2.b0f90b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b139204596da3728a2",
+    "url": "/Wincherz/react_static-list-of-todos/static/js/main.76587a80.chunk.js"
+  },
+  {
+    "revision": "19dbb4b05dc5c23ee6ff",
+    "url": "/Wincherz/react_static-list-of-todos/static/js/runtime-main.b497a53d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "/Wincherz/react_static-list-of-todos/precache-manifest.55d9089eeceee84d59a072b011849d22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wincherz/react_static-list-of-todos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
